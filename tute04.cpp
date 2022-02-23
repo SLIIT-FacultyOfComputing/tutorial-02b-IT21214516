2901518558f766b231fda13edc2c693584d1942c
IT21214516 <99331732+IT21214516@users.noreply.github.com>
2022-02-23 07:41:19 +0000

    Convert C to C++

--- tute04.cpp
+++ tute04.cpp
@@ -16,7 +16,8 @@ Do not modify the main function.*/
 long Factorial(int no);
 long nCr(int n, int r);
 
-int main() {
+int main() 
+{
   int n, r;
   std::cout << "Enter a value for n ";
   std::cin >> n;
@@ -26,4 +27,19 @@ int main() {
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+
+long Factorial(int no)
+{
+	int fac = 1;
+	for (int r = no; r >= 1; r--)
+	{
+		fac = fac * r;
+	}
+	return fac;
+}
+
+long nCr(int n, int r)
+{
+	return Factorial(n) / (Factorial(r) * Factorial(n - r));
 }
